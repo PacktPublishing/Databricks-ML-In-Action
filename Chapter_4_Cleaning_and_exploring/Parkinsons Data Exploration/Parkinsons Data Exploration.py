3d3a3372e83f7a0c254a4e366c5a76d3c60235d8
stephanie.rivera@databricks.com <stephanie.rivera@databricks.com>
2023-08-03 21:20:54 +0000

    Updating ch 4 cleaning with MICE for Parkinsons

--- Chapter_4_Cleaning_and_exploring/Parkinsons Data Exploration/Parkinsons Data Exploration.py
+++ Chapter_4_Cleaning_and_exploring/Parkinsons Data Exploration/Parkinsons Data Exploration.py
@@ -1,5 +1,13 @@
 # Databricks notebook source
-# MAGIC %run ../global-setup $project_name=parkinsons-freezing_gait_prediction
+# MAGIC %run ./../../global-setup $project_name=parkinsons-freezing_gait_prediction $catalog=lakehouse_in_action
+
+# COMMAND ----------
+
+pip install missingno fancyimpute
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
 
 # COMMAND ----------
 
@@ -149,18 +157,111 @@ df_profile.to_file("parkinsons_unlabeled.html")
 
 # MAGIC %md
 # MAGIC
-# MAGIC The Subject & Visit make up the primary key for the Subject table
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC SELECT Subject, Visit, count(*) as count FROM hive_metastore.lakehouse_in_action.parkinsons_subjects GROUP BY Subject, Visit
-
-# COMMAND ----------
-
-# MAGIC %sql
+# MAGIC ## Try out the Databricks Assistant.
+# MAGIC Lets check that Subject & Visit make up the primary key for the Subject table
 # MAGIC
-# MAGIC SELECT StartHesitation, Turn, Walking, Valid, Task, COUNT(*) as cnt FROM hive_metastore.lakehouse_in_action.parkinsons_train_defog GROUP BY ALL ORDER BY cnt DESC
+# MAGIC Let's ask "How many pairs of Subject, Visit are in parkinsons_subjects"
+
+# COMMAND ----------
+
+# DBTITLE 1,This is what I asked for, but not what I wanted.
+# MAGIC %sql
+# MAGIC SELECT COUNT(DISTINCT Subject, Visit) AS pairs_count
+# MAGIC FROM parkinsons_subjects
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Let's try again with better wording "What are the Subject, Visit combinations and how many are there of each?"
+
+# COMMAND ----------
+
+# DBTITLE 1,This is the answer we were looking for.
+# MAGIC %sql
+# MAGIC SELECT Subject, Visit, count(*) as count FROM parkinsons_subjects GROUP BY Subject, Visit
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Asking the same question for the `tdcsfog_metadata` table shows Subject, Visit are not a primary key. In the documentation, it explains that Visit in the `defog_metadata` and the `tdcsfog_metadata` are different column meanings with the same name. 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT
+# MAGIC   Subject,
+# MAGIC   Visit,
+# MAGIC   COUNT(*) AS combo_count
+# MAGIC FROM
+# MAGIC   parkinsons_tdcsfog_metadata
+# MAGIC GROUP BY
+# MAGIC   Subject,
+# MAGIC   Visit
+# MAGIC ORDER BY
+# MAGIC   Subject
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Missing data
+# MAGIC
+# MAGIC Recall from our data profile on the subjects table, UPDRS_Off is 24% null. Lets use the methods [here](https://www.kaggle.com/code/hafsaezzahraouy/dealing-with-missing-data-with-pyspark)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TABLE grouped_subjects AS SELECT * FROM (
+# MAGIC (SELECT 
+# MAGIC   *,
+# MAGIC   'tdcsfog' as FOG_group
+# MAGIC FROM parkinsons_subjects s
+# MAGIC WHERE Subject IN (SELECT DISTINCT Subject FROM parkinsons_tdcsfog_metadata))
+# MAGIC UNION
+# MAGIC (SELECT 
+# MAGIC   *,
+# MAGIC   'defog' as FOG_group
+# MAGIC FROM parkinsons_subjects s
+# MAGIC WHERE Subject IN (SELECT DISTINCT Subject FROM parkinsons_defog_metadata))
+# MAGIC )
+
+# COMMAND ----------
+
+import pandas as pd
+subjects_df = table("grouped_subjects").toPandas()
+subjects_df.info()
+
+# COMMAND ----------
+
+subjects_df.FOG_group = pd.factorize(subjects_df.FOG_group)[0]
+subjects_df.Sex = pd.factorize(subjects_df.Sex)[0]
+
+# COMMAND ----------
+
+import missingno as msno
+msno.bar(subjects_df)
+
+# COMMAND ----------
+
+msno.matrix(subjects_df)
+
+# COMMAND ----------
+
+from fancyimpute import IterativeImputer
+mice_imputed = pd.DataFrame(subjects_df.UPDRSIII_Off).copy(deep=True)
+mice_imputer = IterativeImputer()
+mice_imputed.iloc[:, :] = mice_imputer.fit_transform(mice_imputed)
+
+# COMMAND ----------
+
+subjects_df.UPDRSIII_Off = mice_imputed.UPDRSIII_Off
+
+# COMMAND ----------
+
+msno.matrix(subjects_df)
+
+# COMMAND ----------
+
+spark.createDataFrame(subjects_df).write.mode("overwrite").saveAsTable("silver_subjects")
 
 # COMMAND ----------
 
