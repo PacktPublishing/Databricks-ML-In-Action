f958f4a6b6f63f78b87a18711e0c4727ae2c278f
stephanie.rivera@databricks.com <stephanie.rivera@databricks.com>
2023-10-27 20:25:12 +0000

    cleaning

--- Chapter_6_Searching_for_signal/OpenAI_setup.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-import os
-
-os.environ['OPENAI_API_KEY'] = dbutils.secrets.get(scope="dlia, key="OPENAI_API_KEY")
--- Chapter_6_Searching_for_signal/SQL Chat Bot.py
+++ Chapter_6_Searching_for_signal/SQL Chat Bot.py
@@ -24,26 +24,16 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 
-import sys
-import os
-
-sys.path.append(os.path.abspath('/Repos/stephanie.rivera@databricks.com/Databricks-Lakehouse-ML-in-Action/Chapter_6_Searching_for_signal'))
-
-# COMMAND ----------
-
 # MAGIC %md 
 # MAGIC ### Connect to OpenAI
 
 # COMMAND ----------
 
-# MAGIC %run ./OpenAI_setup
-
-# COMMAND ----------
-
 from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
+import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_KEY'] = dbutils.secrets.get(scope="dlia", key="OPENAI_API_KEY")
 llm = OpenAI(temperature=0)
 chat = ChatOpenAI(model_name="gpt-3.5-turbo")
 
@@ -56,69 +46,24 @@ chat = ChatOpenAI(model_name="gpt-3.5-turbo")
 
 schema = "favorita_forecasting"
 
-# COMMAND ----------
-
 table_schemas = spark.sql(
-    f"""
-    with constraints as (
-      select
-        k.*,
-        cs.constraint_type,
-        u.table_catalog referential_table_catalog,
-        u.table_schema referential_table_schema,
-        u.table_name referential_table_name
-      from
-        system.information_schema.key_column_usage k
-        inner join system.information_schema.table_constraints cs on k.constraint_catalog = cs.constraint_catalog
-        and k.constraint_schema = cs.constraint_schema
-        and k.constraint_name = cs.constraint_name
-        left outer join (
-          select
-            distinct constraint_catalog,
-            constraint_schema,
-            constraint_name,
-            table_catalog,
-            table_schema,
-            table_name
-          from
-            system.information_schema.constraint_column_usage
-        ) u on k.constraint_catalog = u.constraint_catalog
-        and k.constraint_schema = u.constraint_schema
-        and k.constraint_name = u.constraint_name
-        and cs.constraint_type = 'FOREIGN KEY'
-      where
-        k.table_catalog = '{catalog}'
-        and k.table_schema = '{schema}'
-    )
-    select
-  c.*,
-  cs.constraint_name,
-  cs.ordinal_position constraint_ordinal_position,
-  cs.constraint_type,
-  cs.referential_table_catalog,
-  cs.referential_table_schema,
-  cs.referential_table_name
+f"""
+select
+  * 
 from
-  system.information_schema.columns c
-  left outer join constraints cs on c.table_catalog = cs.table_catalog
-  and c.table_schema = cs.table_schema
-  and c.table_name = cs.table_name
-  and c.column_name = cs.column_name
+  system.information_schema.columns 
 where
-  c.table_catalog = '{catalog}'
-  and c.table_schema = '{schema}'
+  table_catalog = '{catalog}'
+  and table_schema = '{schema}'
 order by
   table_name,
   ordinal_position;
-  """
+"""
 )
 
-
-def table_def(table):
-    table_schema = table_schemas.drop(
+table_schemas = table_schemas.drop(
         "table_catalog",
         "table_schema",
-        "table_name",
         "ordinal_position",
         "character_octet_length",
         "numeric_precision",
@@ -135,13 +80,17 @@ def table_def(table):
         "is_system_time_period_start",
         "is_system_time_period_end",
         "system_time_period_timestamp_generation",
-        "is_updatable",
-    ).where(f'table_name = "{table}"')
+        "is_updatable"
+    )
+
+# COMMAND ----------
+
+def table_def(table):
+    table_schema = table_schemas.drop("table_name").where(f'table_name = "{table}"')
 
     return f"Table Schema for {table}: \ncolumn_index" + table_schema.toPandas().to_csv(
         sep="|"
     )
-
 
 def table_records(table):
     records = spark.sql(f"select * from {catalog}.{schema}.{table} limit 2")
@@ -152,37 +101,16 @@ def table_records(table):
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC select
-# MAGIC   *
-# MAGIC from
-# MAGIC   system.information_schema.tables t
-# MAGIC where
-# MAGIC   t.table_catalog = "lakehouse_in_action"
-# MAGIC   and t.table_schema = "favorita_forecasting"
-
-# COMMAND ----------
-
 metadata = {}
-
-sql =f"""
-  select
-      *
-    from
-      system.information_schema.tables t
-    where
-      t.table_catalog = "{catalog}"
-      and t.table_schema = "{schema}"
-    """
-
 table_list = []
-for row in spark.sql(sql).collect():
+
+for row in table_schemas.select("table_name").distinct().collect():
   tbl = row['table_name']
   table_list.append(tbl)
-  meta = table_def(tbl) + '\n\n' + table_records(row['table_name'])
+  meta = table_def(tbl) + '\n\n' + table_records(tbl)
   metadata[tbl] = meta
 
-#print(metadata)
+# print(metadata)
 
 # COMMAND ----------
 
@@ -206,7 +134,8 @@ engine = create_engine(
   connect_args={"http_path": endpoint_http_path, "catalog": catalog}
 )
 
-db = SQLDatabase(engine, schema=None, include_tables=table) # schema=None to work around https://github.com/hwchase17/langchain/issues/2951 ?
+db = SQLDatabase(engine, schema=None, include_tables=table) 
+# schema=None to work around https://github.com/hwchase17/langchain/issues/2951 ?
 db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
 chat_chain = SQLDatabaseChain.from_llm(chat, db, verbose=True)
 
