701df6643b6c562bfe53198d2aceed20047f4401
stephanie.rivera@databricks.com <stephanie.rivera@databricks.com>
2023-12-02 13:49:34 +0000

    adding a rolling time component

--- Chapter_5_Feature_Engineering_on_the_Lakehouse/Project: Synthetic Streaming Transactions Data/Synthetic Data Maximum Price Feature Table.py
+++ Chapter_5_Feature_Engineering_on_the_Lakehouse/Project: Synthetic Streaming Transactions Data/Synthetic Data Maximum Price Feature Table.py
@@ -22,25 +22,32 @@ raw_transactions_df = spark.sql("select * from hive_metastore.lakehouse_prod_in_
 
 # DBTITLE 1,Creating a feature table of product maximum prices.
 from databricks.feature_engineering import FeatureEngineeringClient
-from pyspark.sql.functions import max
-from pyspark.sql.functions import to_date
+from pyspark.sql.functions import max, to_date, col, window, date_trunc, to_timestamp
 
 fe = FeatureEngineeringClient()
 
+time_window = window(
+    col("TransactionTimestamp"),
+    windowDuration="3 hours",
+    slideDuration="1 hour",
+).alias("time_window")
+              
 max_price_df = (
-    raw_transactions_df.withColumn("TransactionDate", to_date("TransactionTimestamp"))
-    .groupBy("Product", "TransactionDate")
-    .agg(max("Amount").alias("MaxProductAmount"))
+    raw_transactions_df.withColumn("TransactionHour", date_trunc('hour',to_timestamp("TransactionTimestamp")))
+    .groupBy(col("Product"),time_window)
+    .agg(max(col("Amount")).alias("MaxProductAmount"))
+    .withColumn("TransactionHour", date_trunc('hour',col("time_window.end")))
+    .drop("time_window")
 )
 
 # Create feature table with Product as the primary key
-# We're using the convention of appending feature table names with "_ft"
-customer_feature_table = fe.create_table(
+# We're using the convention of appending featurlakehouse_in_action.synthetic_transactions.product_max_price_fte table names with "_ft"
+fe.create_table(
   df=max_price_df,
-  name='lakehouse_in_action.synthetic_transactions.product_max_price_ft',
-  primary_keys=['Product','TransactionDate'],
+  name='product_3hour_max_price_ft',
+  primary_keys=['Product','TransactionHour'],
   schema=max_price_df.schema,
-  description='Maximum price per day for Synthetic Transactions products.'
+  description='Maximum price per product over the last 3 hours for Synthetic Transactions.'
 )
 
 # COMMAND ----------
@@ -56,22 +63,22 @@ display(max_price_df)
 
 # DBTITLE 1,Creating a Python UDF and saving to Unity Catalog.
 # MAGIC %sql
-# MAGIC CREATE FUNCTION IF NOT EXISTS product_discount_on_demand_feature(max_price FLOAT, transaction_amount FLOAT)
+# MAGIC CREATE FUNCTION IF NOT EXISTS product_difference_ratio_on_demand_feature(max_price FLOAT, transaction_amount FLOAT)
 # MAGIC RETURNS float
 # MAGIC LANGUAGE PYTHON
-# MAGIC COMMENT 'Calculate the percent discount for a product at time of transaction (maximum price - transaction amount)/maximum price.'
+# MAGIC COMMENT 'Calculate the difference ratio for a product at time of transaction (maximum price - transaction amount)/maximum price.'
 # MAGIC AS $$
-# MAGIC def calc_discount(n1: float, n2: float) -> float:
+# MAGIC def calc_ratio_difference(n1: float, n2: float) -> float:
 # MAGIC   return round(((n1 - n2)/n1),2)
 # MAGIC
-# MAGIC return calc_discount(max_price, transaction_amount)
+# MAGIC return calc_ratio_difference(max_price, transaction_amount)
 # MAGIC $$
 
 # COMMAND ----------
 
 # DBTITLE 1,Testing out the function.
 # MAGIC %sql
-# MAGIC select product_discount_on_demand_feature(15, 100) as discount
+# MAGIC select product_difference_ratio_on_demand_feature(15, 100) as difference_ratio
 
 # COMMAND ----------
 
