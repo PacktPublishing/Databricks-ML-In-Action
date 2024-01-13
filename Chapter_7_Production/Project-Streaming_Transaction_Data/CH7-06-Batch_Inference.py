7b920906fc6daf67d4f717cb616e67aa747bce72
stephanie.rivera@databricks.com <stephanie.rivera@databricks.com>
2024-01-13 17:26:08 +0000

    renaming

--- Chapter_7_Production/Project-Streaming_Transaction_Data/CH7-06-Batch_Inference.py
+++ Chapter_7_Production/Project-Streaming_Transaction_Data/CH7-06-Batch_Inference.py
@@ -115,29 +115,4 @@ fe.delete_feature_serving_endpoint(name=endpoint_name)
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ### Databricks Model Serving
 
-# COMMAND ----------
-
-import os
-import requests
-import numpy as np
-import pandas as pd
-import json
-
-#tf = table format
-def create_tf_serving_json(data):
-  return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
-
-def score_model(dataset):
-  url = 'https://e2-demo-field-eng.cloud.databricks.com/serving-endpoints/TransactionModel/invocations'
-  headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}', 
-'Content-Type': 'application/json'}
-  ds_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
-  data_json = json.dumps(ds_dict, allow_nan=True)
-  response = requests.request(method='POST', headers=headers, url=url, data=data_json)
-  if response.status_code != 200:
-    raise Exception(f'Request failed with status {response.status_code}, {response.text}')
-
-  return response.json()
